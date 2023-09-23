0d1224cdb69ec65fd15bad581c9dff1cf2e791b1
WinterPan2017 <panwentao1301@163.com>
2023-09-23 16:54:54 +0800

    add test for eval and change threshold to 0.5

--- test.py
+++ test.py
@@ -64,7 +64,6 @@ random.seed(32)
 def main():
     params = parser.parse_args()
     epoch_step = json.loads(params.epoch_step)
-    writer = SummaryWriter(os.path.join(params.log_dir, 'LOG', params.name))
 
     in_chn = 1024
 
@@ -85,79 +84,20 @@ def main():
         os.makedirs(params.log_dir)
     log_dir = os.path.join(params.log_dir, 'log.txt')
     save_dir = os.path.join(params.log_dir, 'best_model.pth')
-    z = vars(params).copy()
-    with open(log_dir, 'a') as f:
-        f.write(json.dumps(z))
+
     log_file = open(log_dir, 'a')
+    log_file.write(json.dumps(vars(params).copy()))
 
-    # with open(params.mDATA0_dir_train0, 'rb') as f:
-    #     mDATA_train = pickle.load(f)
-    # with open(params.mDATA0_dir_val0, 'rb') as f:
-    #     mDATA_val = pickle.load(f)
-    # with open(params.mDATA_dir_test0, 'rb') as f:
-    #     mDATA_test = pickle.load(f)
-
-    # SlideNames_train, FeatList_train, Label_train = reOrganize_mDATA(mDATA_train)
-    # SlideNames_val, FeatList_val, Label_val = reOrganize_mDATA(mDATA_val)
-    # SlideNames_test, FeatList_test, Label_test = reOrganize_mDATA_test(mDATA_test)
-
-    # unified dataset
-    SlideNames_train, FeatList_train, Label_train = get_data("train", params.csv_path, params.feature_dir, params.fold, params.nfolds)
-    SlideNames_val, FeatList_val, Label_val = get_data("val", params.csv_path, params.feature_dir, params.fold, params.nfolds)
     SlideNames_test, FeatList_test, Label_test = get_data("test", params.csv_path, params.feature_dir, params.fold, params.nfolds)
 
-    print_log(f'training slides: {len(SlideNames_train)}, validation slides: {len(SlideNames_val)}, test slides: {len(SlideNames_test)}', log_file)
-
-    trainable_parameters = []
-    trainable_parameters += list(classifier.parameters())
-    trainable_parameters += list(attention.parameters())
-    trainable_parameters += list(dimReduction.parameters())
-
-    optimizer_adam0 = torch.optim.Adam(trainable_parameters, lr=params.lr,  weight_decay=params.weight_decay)
-    optimizer_adam1 = torch.optim.Adam(attCls.parameters(), lr=params.lr,  weight_decay=params.weight_decay)
-
-    scheduler0 = torch.optim.lr_scheduler.MultiStepLR(optimizer_adam0, epoch_step, gamma=params.lr_decay_ratio)
-    scheduler1 = torch.optim.lr_scheduler.MultiStepLR(optimizer_adam1, epoch_step, gamma=params.lr_decay_ratio)
-
-    best_auc = 0
-    best_epoch = -1
-    test_auc = 0
-
-    for ii in range(params.EPOCH):
-
-        for param_group in optimizer_adam1.param_groups:
-            curLR = param_group['lr']
-            print_log(f' current learn rate {curLR}', log_file )
-
-        train_attention_preFeature_DTFD(classifier=classifier, dimReduction=dimReduction, attention=attention, UClassifier=attCls, mDATA_list=(SlideNames_train, FeatList_train, Label_train), ce_cri=ce_cri,
-                                                   optimizer0=optimizer_adam0, optimizer1=optimizer_adam1, epoch=ii, params=params, f_log=log_file, writer=writer, numGroup=params.numGroup, total_instance=params.total_instance, distill=params.distill_type)
-        print_log(f'>>>>>>>>>>> Validation Epoch: {ii}', log_file)
-        auc_val = test_attention_DTFD_preFeat_MultipleMean(classifier=classifier, dimReduction=dimReduction, attention=attention,
-                                                           UClassifier=attCls, mDATA_list=(SlideNames_val, FeatList_val, Label_val), criterion=ce_cri, epoch=ii,  params=params, f_log=log_file, writer=writer, numGroup=params.numGroup_test, total_instance=params.total_instance_test, distill=params.distill_type)
-        print_log(' ', log_file)
-        print_log(f'>>>>>>>>>>> Test Epoch: {ii}', log_file)
-        tauc = test_attention_DTFD_preFeat_MultipleMean(classifier=classifier, dimReduction=dimReduction, attention=attention,
-                                                        UClassifier=attCls, mDATA_list=(SlideNames_test, FeatList_test, Label_test), criterion=ce_cri, epoch=ii,  params=params, f_log=log_file, writer=writer, numGroup=params.numGroup_test, total_instance=params.total_instance_test, distill=params.distill_type)
-        print_log(' ', log_file)
-
-        if ii > int(params.EPOCH*0.8):
-            if auc_val > best_auc:
-                best_auc = auc_val
-                best_epoch = ii
-                test_auc = tauc
-                if params.isSaveModel:
-                    tsave_dict = {
-                        'classifier': classifier.state_dict(),
-                        'dim_reduction': dimReduction.state_dict(),
-                        'attention': attention.state_dict(),
-                        'att_classifier': attCls.state_dict()
-                    }
-                    torch.save(tsave_dict, save_dir)
-
-            print_log(f' test auc: {test_auc}, from epoch {best_epoch}', log_file)
-
-        scheduler0.step()
-        scheduler1.step()
+    # final test
+    ck = torch.load(save_dir)
+    classifier.load_state_dict(ck["classifier"])
+    dimReduction.load_state_dict(ck["dim_reduction"])
+    attention.load_state_dict(ck["attention"])
+    attCls.load_state_dict(ck["att_classifier"])
+    tauc = test_attention_DTFD_preFeat_MultipleMean(classifier=classifier, dimReduction=dimReduction, attention=attention,
+                                                        UClassifier=attCls, mDATA_list=(SlideNames_test, FeatList_test, Label_test), criterion=ce_cri, epoch=0,  params=params, f_log=log_file, writer=None, numGroup=params.numGroup_test, total_instance=params.total_instance_test, distill=params.distill_type)
 
 
 def test_attention_DTFD_preFeat_MultipleMean(mDATA_list, classifier, dimReduction, attention, UClassifier, epoch, criterion=None,  params=None, f_log=None, writer=None, numGroup=3, total_instance=3, distill='MaxMinS'):
@@ -275,117 +215,7 @@ def test_attention_DTFD_preFeat_MultipleMean(mDATA_list, classifier, dimReductio
     print_log(f'  First-Tier acc {macc_0}, precision {mprec_0}, recall {mrecal_0}, specificity {mspec_0}, F1 {mF1_0}, AUC {auc_0}', f_log)
     print_log(f'  Second-Tier acc {macc_1}, precision {mprec_1}, recall {mrecal_1}, specificity {mspec_1}, F1 {mF1_1}, AUC {auc_1}', f_log)
 
-    writer.add_scalar(f'auc_0 ', auc_0, epoch)
-    writer.add_scalar(f'auc_1 ', auc_1, epoch)
-
     return auc_1
-
-
-def train_attention_preFeature_DTFD(mDATA_list, classifier, dimReduction, attention, UClassifier,  optimizer0, optimizer1, epoch, ce_cri=None, params=None,
-                                          f_log=None, writer=None, numGroup=3, total_instance=3, distill='MaxMinS'):
-
-    SlideNames_list, mFeat_list, Label_dict = mDATA_list
-
-    classifier.train()
-    dimReduction.train()
-    attention.train()
-    UClassifier.train()
-
-    instance_per_group = total_instance // numGroup
-
-    Train_Loss0 = AverageMeter()
-    Train_Loss1 = AverageMeter()
-
-    numSlides = len(SlideNames_list)
-    numIter = numSlides // params.batch_size
-
-    tIDX = list(range(numSlides))
-    random.shuffle(tIDX)
-
-    for idx in range(numIter):
-
-        tidx_slide = tIDX[idx * params.batch_size:(idx + 1) * params.batch_size]
-
-        tslide_name = [SlideNames_list[sst] for sst in tidx_slide]
-        tlabel = [Label_dict[sst] for sst in tidx_slide]
-        label_tensor = torch.LongTensor(tlabel).to(params.device)
-
-        for tidx, (tslide, slide_idx) in enumerate(zip(tslide_name, tidx_slide)):
-            tslideLabel = label_tensor[tidx].unsqueeze(0)
-
-            slide_pseudo_feat = []
-            slide_sub_preds = []
-            slide_sub_labels = []
-
-            tfeat_tensor = mFeat_list[slide_idx]
-            tfeat_tensor = tfeat_tensor.to(params.device)
-
-            feat_index = list(range(tfeat_tensor.shape[0]))
-            random.shuffle(feat_index)
-            index_chunk_list = np.array_split(np.array(feat_index), numGroup)
-            index_chunk_list = [sst.tolist() for sst in index_chunk_list]
-
-            for tindex in index_chunk_list:
-                slide_sub_labels.append(tslideLabel)
-                subFeat_tensor = torch.index_select(tfeat_tensor, dim=0, index=torch.LongTensor(tindex).to(params.device))
-                tmidFeat = dimReduction(subFeat_tensor)
-                tAA = attention(tmidFeat).squeeze(0)
-                tattFeats = torch.einsum('ns,n->ns', tmidFeat, tAA)  ### n x fs
-                tattFeat_tensor = torch.sum(tattFeats, dim=0).unsqueeze(0)  ## 1 x fs
-                tPredict = classifier(tattFeat_tensor)  ### 1 x 2
-                slide_sub_preds.append(tPredict)
-
-                patch_pred_logits = get_cam_1d(classifier, tattFeats.unsqueeze(0)).squeeze(0)  ###  cls x n
-                patch_pred_logits = torch.transpose(patch_pred_logits, 0, 1)  ## n x cls
-                patch_pred_softmax = torch.softmax(patch_pred_logits, dim=1)  ## n x cls
-
-                _, sort_idx = torch.sort(patch_pred_softmax[:,-1], descending=True)
-                topk_idx_max = sort_idx[:instance_per_group].long()
-                topk_idx_min = sort_idx[-instance_per_group:].long()
-                topk_idx = torch.cat([topk_idx_max, topk_idx_min], dim=0)
-
-                MaxMin_inst_feat = tmidFeat.index_select(dim=0, index=topk_idx)   ##########################
-                max_inst_feat = tmidFeat.index_select(dim=0, index=topk_idx_max)
-                af_inst_feat = tattFeat_tensor
-
-                if distill == 'MaxMinS':
-                    slide_pseudo_feat.append(MaxMin_inst_feat)
-                elif distill == 'MaxS':
-                    slide_pseudo_feat.append(max_inst_feat)
-                elif distill == 'AFS':
-                    slide_pseudo_feat.append(af_inst_feat)
-
-            slide_pseudo_feat = torch.cat(slide_pseudo_feat, dim=0)  ### numGroup x fs
-
-            ## optimization for the first tier
-            slide_sub_preds = torch.cat(slide_sub_preds, dim=0) ### numGroup x fs
-            slide_sub_labels = torch.cat(slide_sub_labels, dim=0) ### numGroup
-            loss0 = ce_cri(slide_sub_preds, slide_sub_labels).mean()
-            optimizer0.zero_grad()
-            loss0.backward(retain_graph=True)
-            torch.nn.utils.clip_grad_norm_(dimReduction.parameters(), params.grad_clipping)
-            torch.nn.utils.clip_grad_norm_(attention.parameters(), params.grad_clipping)
-            torch.nn.utils.clip_grad_norm_(classifier.parameters(), params.grad_clipping)
-            optimizer0.step()
-
-            ## optimization for the second tier
-            gSlidePred = UClassifier(slide_pseudo_feat)
-            loss1 = ce_cri(gSlidePred, tslideLabel).mean()
-            optimizer1.zero_grad()
-            loss1.backward()
-            torch.nn.utils.clip_grad_norm_(UClassifier.parameters(), params.grad_clipping)
-            optimizer1.step()
-
-            Train_Loss0.update(loss0.item(), numGroup)
-            Train_Loss1.update(loss1.item(), 1)
-
-        if idx % params.train_show_freq == 0:
-            tstr = 'epoch: {} idx: {}'.format(epoch, idx)
-            tstr += f' First Loss : {Train_Loss0.avg}, Second Loss : {Train_Loss1.avg} '
-            print_log(tstr, f_log)
-
-    writer.add_scalar(f'train_loss_0 ', Train_Loss0.avg, epoch)
-    writer.add_scalar(f'train_loss_1 ', Train_Loss1.avg, epoch)
 
 
 class AverageMeter(object):
@@ -414,65 +244,6 @@ def print_log(tstr, f):
     print(tstr)
 
 
-def reOrganize_mDATA_test(mDATA):
-
-    # tumorSlides = os.listdir(testMask_dir)
-    # tumorSlides = [sst.split('.')[0] for sst in tumorSlides]
-    # modify for easy get test label
-    df = pd.read_csv("/home/pwt/PosMIL/datasets/camelyon16_resnet50_10fold.csv")
-    tumorSlides = set()
-    for i, row in df.iterrows():
-        if row[2] in [0] and row[1] == 1:
-            tumorSlides.add(row[0])
-
-    SlideNames = []
-    FeatList = []
-    Label = []
-    for slide_name in mDATA.keys():
-        SlideNames.append(slide_name)
-
-        if slide_name in tumorSlides:
-            label = 1
-        else:
-            label = 0
-        Label.append(label)
-
-        patch_data_list = mDATA[slide_name]
-        featGroup = []
-        for tpatch in patch_data_list:
-            tfeat = torch.from_numpy(tpatch['feature'])
-            featGroup.append(tfeat.unsqueeze(0))
-        featGroup = torch.cat(featGroup, dim=0) ## numPatch x fs
-        FeatList.append(featGroup)
-
-    return SlideNames, FeatList, Label
-
-
-def reOrganize_mDATA(mDATA):
-
-    SlideNames = []
-    FeatList = []
-    Label = []
-    for slide_name in mDATA.keys():
-        SlideNames.append(slide_name)
-
-        if slide_name.startswith('tumor'):
-            label = 1
-        elif slide_name.startswith('normal'):
-            label = 0
-        else:
-            raise RuntimeError('Undefined slide type')
-        Label.append(label)
-
-        patch_data_list = mDATA[slide_name]
-        featGroup = []
-        for tpatch in patch_data_list:
-            tfeat = torch.from_numpy(tpatch['feature'])
-            featGroup.append(tfeat.unsqueeze(0))
-        featGroup = torch.cat(featGroup, dim=0) ## numPatch x fs
-        FeatList.append(featGroup)
-
-    return SlideNames, FeatList, Label
 
 def get_data(state, csv_path, feature_dir, fold, nfolds):
     #---->split dataset
